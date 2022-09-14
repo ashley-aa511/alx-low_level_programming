6335491ac61d536de06103513116b91b63e87860
ashley-aa511 <okeyoashley@gmail.com>
2022-09-14 13:04:44 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,17 @@
-#include <main.h>
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Prints _putchar, followed by a new line
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-return (write(1, &c, 1));
+char oli[] = "_putchar";
+int c;
+for (c = 0, c < 8, c++)
+{
+_putchar(oli[c]);
+}
+_putchar('/n');
+return (0);
 }
