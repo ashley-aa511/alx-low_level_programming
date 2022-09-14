4b44e2e0dc0c91693085606cc39614443be4785f
ashley-aa511 <okeyoashley@gmail.com>
2022-09-14 14:17:57 -0700

    task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * main - Prints _putchar, followed by a new line
+ * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char oli[] = "_putchar";
-int c;
-for (c = 0, c < 8, c++)
+char *sh = "_putchar";
+while (*sh)
 {
-_putchar(oli[c]);
+_putchar(*sh);
+sh++;
 }
-_putchar('/n');
+_putchar('\n');
 return (0);
 }
