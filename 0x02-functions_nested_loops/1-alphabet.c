21a1b75af9c695b35d5e7f04b6e440f2ae388234
ashley-aa511 <okeyoashley@gmail.com>
2022-09-14 14:54:12 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,10 +6,10 @@
  */
 void print_alphabet(void)
 {
-int n;  
-for (n = 'a'; n <= 'z'; n++)    
-{      
-_putchar(n);      
-}  
-_putchar('\n');  
+int n;
+for (n = 'a'; n <= 'z'; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
 }
