9906c12b64e526236fabc4a1855bd91ef1a69438
ashley-aa511 <okeyoashley@gmail.com>
2022-12-31 01:44:59 -0800

    tasks

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,11 @@
-#ifndef HOLBERTON_H
-#define MAIN_H
+#ifndef MAIN_H
+#define MAINN_H
+/*
+ * File: main.h
+ * Auth: ashley
+ * Desc: Header file containing prototypes for all functions
+ *       written in the 0x14-file_io directory.
+ */
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
@@ -7,4 +13,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-#endif /* MAIN_H */
+#endif /* MAIN_H*/
