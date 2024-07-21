dc7b671ade2d1f7a96f8e1177f6e23dcd273e01e
ashley-aa511 <okeyoashley@gmail.com>
2024-07-21 03:46:59 -0700

    betty configuration

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
  */
 int main(void)
 {
-  char l;
-  for (l = 'a'; l <= 'z'; l++){
-    putchar(l);
-  }
-  for (l = 'A'; l <= 'Z'; l++){
-    putchar(l);
-  }
-  putchar('\n');
-  return 0;
+    char l;
+    for (l = 'a'; l <= 'z'; l++) {
+        putchar(l);
+    }
+    for (l = 'A'; l <= 'Z'; l++) {
+        putchar(l);
+    }
+    putchar('\n');
+    return 0;
 }
